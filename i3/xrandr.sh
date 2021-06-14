5e529a152b7e8fd20e7ab56b40c3a7d69b6fcd08
Christof Duhme <christof.duhme@gmail.com>
2021-06-14 16:59:17 +0200

    fix new monitor resolution

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0.8 --mode 2560x1440 --pos 1920x0 --rotate normal
+xrandr --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-0.8 --mode 2560x1440 --pos 0x0 --rotate normal
